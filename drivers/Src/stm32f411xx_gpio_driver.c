401bce3d6a662c7fd9e83a17114ca092fcc3f60a
[Rémi Boulière] <remi.bouliere83@gmail.com>
2023-05-28 21:03:18 +0200

    Documentation for GPIO functions

--- drivers/Src/stm32f411xx_gpio_driver.c
+++ drivers/Src/stm32f411xx_gpio_driver.c
@@ -9,68 +9,188 @@
 #include "stm32f411xx_gpio_driver.h"
 
 
-/*
- * Peripherical Clock Setup
- */
+
+/*****************************************************************
+ * @fn          - GPIO_PeriClockControl
+ *
+ * @brief       - This function enables or disables peripheral
+ *                clock for the given GPIO port
+ *
+ * @param[in]   - Base address of the GPIO peripheral
+ * @param[in]   - Macros: Enable or Disable
+ *
+ * @return      - None
+ *
+ * @Note        - None
+ *
+ *****************************************************************/
 
 void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnOrDi)
 {
 
 }
 
-/*
- * Init and De-Init
- */
 
+/*****************************************************************
+ * @fn          - GPIO_Init
+ *
+ * @brief       - This function initialize GPIO peripherals
+ *
+ * @param[in]   - Pointer to GPIO Handle structure
+ *
+ * @return      - None
+ *
+ * @Note        - None
+ *
+ *****************************************************************/
 void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
 {
 
 }
 
+/*****************************************************************
+ * @fn          - GPIO_DeInit
+ *
+ * @brief       - This function de-initialize GPIO peripherals
+ *
+ * @param[in]   - Base address of the GPIO peripheral
+ *
+ * @return      - None
+ *
+ * @Note        - None
+ *
+ *****************************************************************/
 void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
 {
 
 }
 
-/*
- * Read/Write
- */
-
+/*****************************************************************
+ * @fn          - GPIO_ReadFromInputPin
+ *
+ * @brief       - This function reads value of input pin, on
+ *                a specific port
+ *
+ * @param[in]   - Base address of the GPIO peripheral
+ * @param[in]   - Pin number
+ *
+ * @return      - Content of the input data
+ *
+ * @Note        - 0 or 1
+ *
+ *****************************************************************/
 uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
 {
 
 }
 
+/*****************************************************************
+ * @fn          - GPIO_ReadFromInputPort
+ *
+ * @brief       - This function reads value of input port
+ *
+ * @param[in]   - Base address of the GPIO peripheral
+ *
+ * @return      - Content of the input data
+ *
+ * @Note        - None
+ *
+ *****************************************************************/
 uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx)
 {
 
 }
 
+
+/*****************************************************************
+ * @fn          - GPIO_WriteToOutputPin
+ *
+ * @brief       - This function writes value on a specific
+ *                output pin
+ *
+ * @param[in]   - Base address of the GPIO peripheral
+ * @param[in]   - Pin number
+ * @param[in]   - Value (Set/Reset Macro)
+ *
+ * @return      - None
+ *
+ * @Note        - None
+ *
+ *****************************************************************/
 void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t value)
 {
 
 }
 
+/*****************************************************************
+ * @fn          - GPIO_WriteToOutputPort
+ *
+ * @brief       - This function writes value on a specific
+ *                output port
+ *
+ * @param[in]   - Base address of the GPIO peripheral
+ * @param[in]   - Value (Set/Reset Macro)
+ *
+ * @return      - None
+ *
+ * @Note        - None
+ *
+ *****************************************************************/
 void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t value)
 {
 
 }
 
+/*****************************************************************
+ * @fn          - GPIO_ToggleOutputPin
+ *
+ * @brief       - This function toggles specific output pin
+ *
+ * @param[in]   - Base address of the GPIO peripheral
+ * @param[in]   - Pin number
+ *
+ * @return      - None
+ *
+ * @Note        - None
+ *
+ *****************************************************************/
 void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
 {
 
 }
 
 
-/*
- * IRQ Handling
- */
-
+/*****************************************************************
+ * @fn          - GPIO_IRQConfig
+ *
+ * @brief       - This function configures interrupt
+ *
+ * @param[in]   - IRQ Interrupt number
+ * @param[in]   - Macro: Enable/Disable
+ *
+ * @return      - None
+ *
+ * @Note        - None
+ *
+ *****************************************************************/
 void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnOrDi)
 {
 
 }
 
+/*****************************************************************
+ * @fn          - GPIO_IRQPriorityConfig
+ *
+ * @brief       - This function configures interrupt priority
+ *
+ * @param[in]   - IRQ Interrupt number
+ * @param[in]   - IRQ interrupt priority
+ *
+ * @return      - None
+ *
+ * @Note        - None
+ *
+ *****************************************************************/
 void GPIO_IRQHandling(uint8_t PinNumber)
 {
 
